9eda74cf88a47149be1aa8ebec052bf7c02bdba8
Marcos Ferreira Tavares <marcosprogramadorti@gmail.com>
2020-02-09 00:19:55 -0300

    Tirando RequestMapping db

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -53,27 +53,27 @@ public class Main {
     return "index";
   }
 
-  @RequestMapping("/db")
-  String db(Map<String, Object> model) {
-    try (Connection connection = dataSource.getConnection()) {
-      Statement stmt = connection.createStatement();
-      //stmt.executeUpdate("CREATE TABLE IF NOT EXISTS pessoas (tick timestamp)");
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
-      ArrayList<String> output = new ArrayList<String>();
-      while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
-      }
-
-      model.put("records", output);
-      return "db";
-    } catch (Exception e) {
-      model.put("message", e.getMessage());
-      return "error";
-    }
-  }
+//  @RequestMapping("/db")
+//  String db(Map<String, Object> model) {
+//    try (Connection connection = dataSource.getConnection()) {
+//      Statement stmt = connection.createStatement();
+//      //stmt.executeUpdate("CREATE TABLE IF NOT EXISTS pessoas (tick timestamp)");
+//      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+//      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+//      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+//
+//      ArrayList<String> output = new ArrayList<String>();
+//      while (rs.next()) {
+//        output.add("Read from DB: " + rs.getTimestamp("tick"));
+//      }
+//
+//      model.put("records", output);
+//      return "db";
+//    } catch (Exception e) {
+//      model.put("message", e.getMessage());
+//      return "error";
+//    }
+//  }
 
 
 
