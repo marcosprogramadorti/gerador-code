4692dbffde14fcd4049cd9298fd8b657377587f2
Marcos Ferreira Tavares <marcosprogramadorti@gmail.com>
2020-02-09 00:12:14 -0300

    RestController - pessoa

---
+++ src/main/java/com/controllers/PessoaController.java
@@ -0,0 +1,25 @@
+package com.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Pessoa;
+import com.repositorys.IPessoa;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+	
+	@Autowired
+    private IPessoa pessoaRepository;
+	
+	@GetMapping("/todas")
+	public List<Pessoa> getPessoas() {
+        return pessoaRepository.findAll();
+    }
+
+}
