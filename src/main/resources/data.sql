075036519462fff07a55925d7568aebd8d0f6b53
Marcos Ferreira Tavares <marcosprogramadorti@gmail.com>
2020-02-09 13:48:29 -0300

    Comentado data.sql.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO pessoa (ID_PESSOA, CPF, NOME) VALUES
+/*INSERT INTO pessoa (ID_PESSOA, CPF, NOME) VALUES
   ('1', '123', 'Marcos'),
   ('2', '432', 'Marly'),
-  ('3', '567', 'Maria');
+  ('3', '567', 'Maria');*/
