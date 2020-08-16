fa02573e93c710ffe35ed18028d12e75467c50fc
unknown <marcosprogramadorti@gmail.com>
2020-08-16 12:17:45 -0300

    auto incremento - a

--- src/main/java/com/gerador/entidades/Menu.java
+++ src/main/java/com/gerador/entidades/Menu.java
@@ -5,19 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
-
-import org.hibernate.annotations.Generated;
-import org.hibernate.annotations.GenerationTime;
 
 @Entity
 @Table(name = "menu")
 public class Menu {
 
 	@Id
-	@SequenceGenerator(name = "pk_sequence", sequenceName = "menu_menu_id_seq", allocationSize = 1)
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pk_sequence")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "menu_id", unique = true, nullable = false)
 	private Long idMenu;
 
