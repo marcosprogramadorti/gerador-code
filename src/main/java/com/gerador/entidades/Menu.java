f547d904617a82b2627252fbe445e5be6526acfd
unknown <marcosprogramadorti@gmail.com>
2020-08-16 17:08:27 -0300

    novo modelo dados

--- src/main/java/com/gerador/entidades/Menu.java
+++ src/main/java/com/gerador/entidades/Menu.java
@@ -18,6 +18,24 @@ public class Menu {
 	@OneToMany
 	@JoinColumn(name="id_menu")
 	private List<Data> data;
+
+	public Long getIdMenu() {
+		return idMenu;
+	}
+
+	public void setIdMenu(Long idMenu) {
+		this.idMenu = idMenu;
+	}
+
+	public List<Data> getData() {
+		return data;
+	}
+
+	public void setData(List<Data> data) {
+		this.data = data;
+	}
+	
+	
 	
 
 }
