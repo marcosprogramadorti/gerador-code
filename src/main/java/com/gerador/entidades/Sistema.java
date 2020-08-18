2d605e13b7231cc74972ce9e52a9198d119cccc1
Marcos Tavares <marcosprogramadorti@gmail.com>
2020-08-18 08:56:28 -0300

    seq id teste

--- src/main/java/com/gerador/entidades/Sistema.java
+++ src/main/java/com/gerador/entidades/Sistema.java
@@ -1,9 +1,12 @@
 package com.gerador.entidades;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -11,6 +14,8 @@ import javax.persistence.Table;
 public class Sistema {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sistema_seq_gen")
+	@SequenceGenerator(name = "sistema_seq_gen", sequenceName = "sistema_id_seq")
 	private Long idSistema;
 	private String contexto;
 	private String sigla;
